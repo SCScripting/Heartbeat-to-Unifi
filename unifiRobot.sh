e4d43565db93356cbd790cb406965e0950e378ce
SCScripting <staff@standardcomputer.com>
2026-03-11 11:24:02 -0500

    Update unifiRobot.sh
    Updated for wider version adaption

--- unifiRobot.sh
+++ unifiRobot.sh
@@ -6,8 +6,9 @@
 cd /data/
 
 # Create cronjob cURL location
-mkdir cronjobs
-touch /cronjobs/healthchecksio
+mkdir /data/cronjobs
+touch /data/cronjobs/healthchecksio
+cd /data/
 
 # Grab the cronjob check
 curl https://raw.githubusercontent.com/SCScripting/Unifi-Heartbeat-Cronjob-Script/refs/heads/main/25-add-cron-jobs.sh --output 25-add-cron-jobs.sh
@@ -15,7 +16,6 @@ echo "Enter Heartbeat: "
 read heartbeat
 
 # Creates the unique heartbeat cURL for the cronjob
-
 cat << 'EOF' > /cronjobs/healthchecksio
 * * * * * root curl -fsL "$heartbeat" >/dev/null 2>&1
 EOF
